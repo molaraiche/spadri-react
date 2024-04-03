bc1326ed33a2d41a13f8d79406536456598f4b6e
molaraiche <laraichemohamed@gmail.com>
2024-04-03 05:26:24 +0000

    updates

--- src/admin/ManageProducts.tsx
+++ src/admin/ManageProducts.tsx
@@ -1,10 +1,48 @@
 import { Link } from 'react-router-dom';
+import '../styles/table.css';
+import { MdModeEditOutline, MdDelete } from 'react-icons/md';
 
 const ManageProducts = () => {
   return (
-    <div>
+    <div className='productManage'>
       <div className='BackBtn'>
         <Link to='/dashboard'>Back</Link>
+      </div>
+      <div className='addNewProduct'>
+        <button> Add new product</button>
+      </div>
+      <div className='productsManageSection'>
+        <table border={2}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Image</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Path</th>
+              <th>Manage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Description</td>
+              <td>Image</td>
+              <td>Quantity</td>
+              <td>Price</td>
+              <td>Path</td>
+              <td className='btnHolder'>
+                <button className='editBtn'>
+                  <MdModeEditOutline />
+                </button>
+                <button className='deleteBtn'>
+                  <MdDelete />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
